09afead4223fb16bc6c788e4f10e64453b863d25
cat <pdb1313@naver.com>
2023-05-16 17:45:56 +0900

    w

--- 2rd/fe/src/client/js/default.js
+++ 2rd/fe/src/client/js/default.js
@@ -38,8 +38,11 @@ function hoverOnlyGnb() {
             }
         })
     }
-    function leave_listener() {
+    function leave_listener( e ) {
+        let parent = e.currentTarget.parent;
+        console.log(parent)
     }
+
 
 
 
@@ -49,7 +52,6 @@ function hoverOnlyGnb() {
             elem.addEventListener("mouseenter", enter_listener);
             elem.addEventListener("mouseleave", leave_listener);
         })
-        
     }
 
     function init() {
@@ -59,5 +61,87 @@ function hoverOnlyGnb() {
         init();
     })()
 }
+function hoverListActive() {
+    function enter_listener() {
+        // console.log(btns)
+        btns.forEach(( elem, i )=>{
+            if(idx == i) {
+                elem.classList.add("on")
+            } else {
+                elem.classList.remove("on")
+            }
+        })
+    }
+    function append_event( e ) {
+        // console.log(e.currentTarget)
+        idx = btns.indexOf(e.currentTarget);
+        // console.log(idx)
+        enter_listener();
+    }
+    function leave_event(e) {
+        // console.log("vvv")
+        // console.log(e)
+        let parent = e.currentTarget.parentElement;
+        // console.log(parent)
+        if(!parent.contains(e.currentTarget)){
+            console.log("aaa")
+        }
+    }
+
+    function event_listener() {
+        // console.log(btns)
+        btns.forEach(( elem, i )=>{
+            elem.addEventListener("mouseenter", append_event);
+            elem.addEventListener("mouseleave", leave_event);
+        })
+    }
+    (function() {
+        var prd_slide = new Swiper(".prd-slide", {
+            slidesPerView: 4,
+            spaceBetween: 30,
+            pagination: {
+                clickable: true,
+              },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              },
+            breakpoints: {
+                1300: {
+                    slidesPerView: 4,  //브라우저가 1024보다 클 때
+                  },
+                1200: {
+                    slidesPerView: 3,  //브라우저가 1024보다 클 때
+                  },
+                920: {
+                    slidesPerView: 3,  //브라우저가 1024보다 클 때
+                  },
+                640: {
+                    slidesPerView: 2,  //브라우저가 1024보다 클 때
+                  },
+
+                0: {
+                    slidesPerView: 1,  //브라우저가 768보다 클 때
+                  },
+            },
+            on : {
+                init : function() {
+                    // console.log("init")
+                    btns = Array.prototype.slice.call(document.querySelectorAll(".prd-list li"));
+                    event_listener();
+                }
+            }
+          });
+    })();
+}
+
+
 new toggleVideo;
 new hoverOnlyGnb;
+new hoverListActive;
+
+/**
+ * Swiper
+ */
+
+
