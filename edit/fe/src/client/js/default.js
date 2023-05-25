c5e4579eeef8999f3941065bb56f2e7028a9a4b0
cat <pdb1313@naver.com>
2023-05-25 21:00:48 +0900

    s

--- edit/fe/src/client/js/default.js
+++ edit/fe/src/client/js/default.js
@@ -1,40 +1,16 @@
 function hoverGnb() {
     let
         btns = Array.prototype.slice.call(document.querySelectorAll(".gnb-box"))
-    ,   contents = Array.prototype.slice.call(document.querySelectorAll(".sub-menu"))
-    ,   idx = 0
     ;
 
-
-    function show_layer() {
-        btns.forEach(function( elem, i ) {
-            if ( idx == i ) {
-                // console.log("dsdd")
-                contents[i].classList.add("on");
-            } else {
-                contents[i].classList.remove("on");
-            }
-        })
-    }
-
-    function append_event( e ) {
-        //console.log( e )
-        idx = btns.indexOf(e.currentTarget);
-        //console.log(idx)
-        show_layer(e);
-        
-    }
-    function leave_listener( e ) {
-        contents.forEach(function( content ) {
-            // console.log(content)
-            content.classList.remove("on")
-        })
-    }
-
     function event_listener() {
-        btns.forEach(function(elem, i) {
-            elem.addEventListener("mouseenter", append_event);
-            elem.addEventListener("mouseleave", leave_listener);
+        if ( btns ) btns.forEach(function(elem, i) {
+            elem.addEventListener("mouseenter", ( e )=>{
+                elem.classList.add("on");
+            });
+            elem.addEventListener("mouseleave",  ( e )=>{
+                elem.classList.remove("on");
+            });
         });
     };
 
@@ -46,33 +22,179 @@ function hoverGnb() {
         init();
     })();
 }
+function changeInputValue() {
+    let 
+        // pw-i를 누르면 required의 type ps or text change
+        btns = Array.prototype.slice.call(document.querySelectorAll(".pw-i"))
+    ,   icons = Array.prototype.slice.call(document.querySelectorAll(".required"))
+    ;
 
-/*
-const observer_node = function( callback, target_node = document.querySelector("body"), config = { attributes: true, childList: true, subtree: true, characterDataOldValue: true } ) {
-    const observer = new MutationObserver( callback );
-    observer.observe( target_node, config );
- }
- */
+    function append_event( e ) {
+        // console.log("Aaaa")
+        // console.log(e.currentTarget)
+        idx = btns.indexOf(e.currentTarget);
+        // console.log(idx);
+        btns.forEach(( btn, i ) => {
+            if ( idx == i ) {
+                btn.classList.toggle("on")
+                if ( icons[i].type == 'password' ) {
+                    icons[i].type = 'text'
+                } else if ( icons[i].type == 'text' ) {
+                    icons[i].type = 'password'
+                }
+            }
+        })
+    }
 
-new hoverGnb;
+    function event_listener() {
+        // console.log(icons, btns)
+        if ( btns.length > 0 ) {
+            btns.forEach(( btn, i )=> {
+                btn.addEventListener("click", append_event);
+            })
+        }
+    }
+    function init() {
+        event_listener();
+    }
+    (function() {
+        init();
+    })();
+    return {
+        init
+    };
+}
+function inputNumber() {
+    let inputs = Array.prototype.slice.call(document.querySelectorAll(".only-num"));
+    
+    function down_listener( e ) {
+        let excusal = ['Backspace','ArrowUp','ArrowDown','ArrowLeft','ArrowRight','Home','End','Tab','Delete','Insert','Ctrl','v'];
+        // console.log(excusal)
+        // console.log(e.key)
+        // e.key >= 0 && e.key <= 9 라면 트루 그 외 false
+        // console.log( excusal.indexOf(e.key) == -1 )
+        if ( !(e.key >= 0 && e.key <= 9) && excusal.indexOf(e.key) == -1 ) {
+            e.preventDefalut;
+            alert("잘못된 입력입니다!!");
+        };
+    }
+    
+    function change_listener( e ) {
+        // console.log(e.currentTarget)
+        // keyup 이 되면 정규식 치환, 숫자 제외하고 다
+        let val = e.target.value.replace(/[^0-9,-]/g,"");
+        // console.log(val)
+        e.target.value = val;
+        // console.log(val)
+    }
 
-/*
-function clicka() {
-    const btns = document.querySelectorAll(".btn:not([data-event-ok=true])");
-    btns.forEach(( btn ) => {
-        btn.dataset.eventOk = "true";
-        btn.addEventListener("click", function() {
-            alert("눌름");
-            document.querySelector("footer").innerHTML = "<button type='button' class='btn'>bbb</button>";
+    function event_listener() {
+        // console.log("Aaaa")
+        inputs.forEach(( elem, i )=>{
+            elem.addEventListener("keyup", change_listener);
+            elem.addEventListener("change", change_listener);
+            elem.addEventListener("keydown", down_listener);
+        })
+    }
+    function init() {
+        if ( inputs.length > 0 ) event_listener();
+    }
+    return init();
+}
+function activeIcon() {
+    let 
+        btns = document.querySelector(".input")
+    ,   actives = Array.prototype.slice.call(document.querySelectorAll(".process-pw li"))
+    ;
+
+    function press_event( e ) {
+        // console.log("aaaaaaa")
+        actives.forEach(( elem, i )=>{
+            if ( e.target.value.length > i ) {
+                elem.classList.add("active");
+            } else {
+                elem.classList.remove("active");
+            }
+        })
+    }
+
+    function event_listener() {
+        if ( btns ) btns.addEventListener("keyup", press_event);
+    }
+    function init() {
+        event_listener();
+    }
+    return init();
+}
+function inpCreditNum() {
+    let inputs = Array.prototype.slice.call(document.querySelectorAll(".input-number"));
+    function key_listener( e ) {
+        // e.length이 maxlength 숫자형보다 크거나 같을때
+        // console.log(e.target.value.length, e.target.getAttribute("maxlength")*1)
+        if ( e.target.value.length >= e.target.getAttribute("maxlength") * 1 ) {
+            let idx = inputs.indexOf(e.target);
+            // console.log(idx)
+            if ( inputs[idx + 1] ) {
+                inputs[idx + 1].select();
+                inputs[idx + 1].focus();
+            }
+        }
+        // inp들이 select(), focus() 이벤트가 일어난다
+    }
+    function event_listener() {
+        inputs.forEach(( inp )=>{
+            inp.addEventListener("keyup", key_listener);
+        })
+    }
+    function init() {
+        event_listener();
+    }
+    return init();
+}
+function chkActive() {
+    let 
+        chks = Array.prototype.slice.call(document.querySelectorAll(".chks"))
+    ,   all = document.querySelector(".all-chk")
+    ;
+
+    function all_chk( e ) {
+        chks.forEach(function( chk ) {
+			chk.checked = e.target.checked;
+		})
+    }
+
+    function chk_count( e ) {
+        let chk = 0;
+        chks.forEach(( elem )=> {
+            if ( elem.checked == true ) chk += 1;
+            // console.log(chk)
         });
-    });
+        console.log(chks.length, chk);
+
+        if ( chks.length == chk ) all.checked = true;
+        else all.checked = false;
+    }
+
+    function event_listener() {
+        chks.forEach(( chk ) => {
+            chk.addEventListener("change", chk_count);
+        })
+        if (all) all.addEventListener("change", all_chk);
+    }
+    function init() {
+        event_listener();
+    }
+    (function() {
+        init();
+    })();
 }
 
-observer_node( clicka );
+new hoverGnb;
+new changeInputValue;
+new inputNumber;
+new activeIcon;
+new inpCreditNum;
+new chkActive;
 
 
 
-observer_node(function() {
-    console.log("변해따");
-});
-*/
--- todolist/fe/src/client/js/default.js
+++ todolist/fe/src/client/js/default.js
@@ -32,6 +32,7 @@ function todo() {
         card.classList.add("card-box");
         cardtxt.classList.add("card-content");
         cardtxt.innerText = newList.text;
+        li.setAttribute("draggable", true);
         card.appendChild(cardtxt);
         card.appendChild(deleteButton);
         li.appendChild(card);
@@ -70,7 +71,64 @@ function todo() {
         event_listener();
     }
     (function() {
-        init();
-    })()
+        init()
+    })();
 }
+
+function dragNDrop() {
+    const  list = document.querySelectorAll(".todo-list li");
+    function move_listener( e ) {
+        if ( e.touches ) e = e.touches[0];
+        let elem = obj.targetElem;
+
+        elem.style.setProperty("left", `${e.pageX - obj.x}px`);
+        elem.style.setProperty("top", `${e.pageY - obj.y}px`);
+        // console.log(elem) 따라오는거 확인
+    }
+    function mouseup_listener( e ) { 
+        e.target.classList.remove("dragActive");
+        window.removeEventListener("mousemove", move_listener);
+        window.removeEventListener("touchmove", move_listener);
+        window.removeEventListener("mouseup", mouseup_listener);
+        window.removeEventListener("touchend", mouseup_listener);
+    }
+
+    function mousedown_listener( e ) {
+        let c_target = e.currentTarget;
+        
+        if ( e.touches ) e = e.touches[0];
+
+        obj = {
+            targetElem: c_target,
+            x: ( e.pageX - c_target.getBoundingClientRect().x ),
+            y: ( e.pageY - c_target.getBoundingClientRect().y )
+        };
+        // console.log(e.pageX,e.pageY); 마우스 좌표
+        // console.log(c_target.getBoundingClientRect().x, c_target.getBoundingClientRect().y); 현재 타켓이 화면 왼쪽으로부터 상대적 위치 값
+        // console.log(obj);
+        c_target.classList.add("dragActive");
+        window.addEventListener("mousemove", move_listener);
+        window.addEventListener("touchmove", move_listener);
+        window.addEventListener("mouseup", mouseup_listener);
+        window.addEventListener("touchend", mouseup_listener);
+    }
+    
+    function event_listener() {
+        if ( list.length > 0 ) {
+            list.forEach(( elem ) => {
+                elem.addEventListener("mousedown", mousedown_listener);
+                elem.addEventListener("touchstart", mousedown_listener);
+            })
+        }
+    }
+    function init() {
+        event_listener();
+    }
+    (function() {
+        init()
+    })();
+}
+
+
 new todo;
+new dragNDrop;
